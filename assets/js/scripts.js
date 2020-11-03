b0fd08e77ab023989267026d4188ff35236a7b72
Yasmine091 <yasmine.emdounan@gmail.com>
2020-11-03 21:39:46 +0100

    Grosse màj - #oubli

---
+++ assets/js/scripts.js
@@ -0,0 +1,62 @@
+const task = document.getElementById("nwtask").value;
+const list = document.getElementById("mylist");
+const todo = document.querySelectorAll("li");
+
+function deltask(event) {
+    var target = event.target;
+    var parent = target.parentElement;
+    $(parent).remove();
+}
+
+function chktask(event) {
+    var target = event.target;
+    var parent = target.parentElement;
+    parent.classList.add("done");
+}
+
+function charged() {
+    $.getJSON("assets/js/tasks.json", function(tasks) {
+        var len = tasks.length;
+
+        for (i = 0; i < len; i++) {
+            tasks == tasks[i];
+
+            var li = document.createElement("li");
+
+            li.innerHTML =
+                '<input type="checkbox" class="btf">' +
+                '<label>' + tasks[i].desc +
+                '</label><span onclick="deltask()">x</span>';
+
+            list.appendChild(li);
+            document.querySelector(span).addEventListener('click', deltask);
+            li.addEventListener('click', chktask);
+        }
+
+    })
+
+};
+
+
+
+function addtsk() {
+    let task = document.getElementById("nwtask").value;
+    let list = document.getElementById("mylist");
+    let li = document.createElement("li");
+
+    if (!task) {
+        return;
+    }
+
+    li.innerHTML =
+        '<input type="checkbox" class="btf">' +
+        '<label>' + task +
+        '</label><span onclick="deltask()">x</span>';
+
+    list.appendChild(li);
+    document.querySelector(span).addEventListener('click', deltask);
+    li.addEventListener('click', chktask);
+
+}
+
+task.addEventListener('click', addtsk);
