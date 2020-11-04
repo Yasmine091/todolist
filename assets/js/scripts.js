e9b683f8d595c8ad49b7bc7f4acb17b90ee0023e
Yasmine091 <yasmine.emdounan@gmail.com>
2020-11-04 09:45:37 +0100

    Màj de tous les fichiers -  #oubli

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,8 +14,32 @@ function chktask(event) {
     parent.classList.add("done");
 }
 
+
+function addtsk() {
+    let task = document.getElementById("nwtask").value;
+    let list = document.getElementById("mylist");
+    let li = document.createElement("li");
+
+    if (!task) {
+        return;
+    }
+
+    li.innerHTML =
+        '<input type="checkbox">' +
+        '<label>' + task +
+        '</label><span onclick="deltask()">x</span>';
+
+    list.appendChild(li);
+    document.querySelector('span').addEventListener('click', deltask);
+    li.addEventListener('click', chktask);
+
+}
+
+task.addEventListener('click', addtsk);
+
 function charged() {
     $.getJSON("assets/js/tasks.json", function(tasks) {
+        let list = document.getElementById("mylist");
         var len = tasks.length;
 
         for (i = 0; i < len; i++) {
@@ -29,34 +53,9 @@ function charged() {
                 '</label><span onclick="deltask()">x</span>';
 
             list.appendChild(li);
-            document.querySelector(span).addEventListener('click', deltask);
+            document.querySelector('span').addEventListener('click', deltask);
             li.addEventListener('click', chktask);
         }
 
     })
-
 };
-
-
-
-function addtsk() {
-    let task = document.getElementById("nwtask").value;
-    let list = document.getElementById("mylist");
-    let li = document.createElement("li");
-
-    if (!task) {
-        return;
-    }
-
-    li.innerHTML =
-        '<input type="checkbox" class="btf">' +
-        '<label>' + task +
-        '</label><span onclick="deltask()">x</span>';
-
-    list.appendChild(li);
-    document.querySelector(span).addEventListener('click', deltask);
-    li.addEventListener('click', chktask);
-
-}
-
-task.addEventListener('click', addtsk);
